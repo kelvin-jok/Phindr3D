6068c6f32057d6d6fd2c8c365f198a2536b3143a
kelvin-jok <kelvinwingheen.jok@sri.utoronto.ca>
2022-06-30 16:50:50 -0400

    export/import option for data-points

--- Phindr3D-Python/src/GUI/windows/resultsWindow.py
+++ Phindr3D-Python/src/GUI/windows/resultsWindow.py
@@ -23,6 +23,7 @@ class resultsWindow(QDialog):
         self.setWindowTitle("Results")
         self.feature_file=[]
         self.imageIDs=[]
+        self.plots=[]
         menubar = QMenuBar()
         file = menubar.addMenu("File")
         inputfile = file.addAction("Input Feature File")
@@ -46,6 +47,8 @@ class resultsWindow(QDialog):
         box = QGroupBox()
         boxlayout = QGridLayout()
         selectfile = QPushButton("Select Feature File")
+        prevdata = QPushButton("Import Previous Plot Data")
+        exportdata = QPushButton("Export Plot Data")
         map_type = QComboBox()
         map_type.addItem("PCA")
         map_type.addItem("t-SNE")
@@ -59,14 +62,19 @@ class resultsWindow(QDialog):
         dimensionbox.setLayout(dimensionboxlayout)
         colordropdown = QComboBox()
         boxlayout.addWidget(selectfile, 0, 0, 3, 1)
+
         boxlayout.addWidget(QLabel("Plot Type"), 0, 1, 1, 1)
         boxlayout.addWidget(map_type, 1, 1, 1, 1)
         boxlayout.addWidget(dimensionbox, 2, 1, 1, 1)
         boxlayout.addWidget(QLabel("Color By"), 0, 2, 1, 1)
         boxlayout.addWidget(colordropdown, 1, 2, 1, 1)
+        boxlayout.addWidget(prevdata, 2, 2, 1, 1)
+        boxlayout.addWidget(exportdata, 3, 2, 1, 1)
         box.setLayout(boxlayout)
         #menu actions activated
-        inputfile.triggered.connect(lambda: self.loadFeaturefile(colordropdown, map_type.currentText()))
+        inputfile.triggered.connect(lambda: self.loadFeaturefile(colordropdown, map_type.currentText(), True))
+        exportdata.clicked.connect(lambda: self.save_file(map_type.currentText()))
+        prevdata.clicked.connect(lambda: self.import_file(map_type, colordropdown, threed))
         #setup Matplotlib
         matplotlib.use('Qt5Agg')
         # test points. normally empty list x=[], y=[], z=[] #temporary until read in formated super/megavoxel data
@@ -105,13 +113,13 @@ class resultsWindow(QDialog):
                 self.projection = dim
                 self.main_plot.axes.get_zaxis().line.set_linewidth(1)
                 self.main_plot.axes.tick_params(axis='z', labelsize=10)
-                self.main_plot.fig.canvas.draw()
+                self.main_plot.draw()
                 self.main_plot.axes.mouse_init()
             if self.feature_file and colordropdown.count() > 0:
                 self.data_filt(colordropdown, "False", self.projection, plot, True)
 
         # button features go here
-        selectfile.clicked.connect(lambda: self.loadFeaturefile(colordropdown, map_type.currentText()))
+        selectfile.clicked.connect(lambda: self.loadFeaturefile(colordropdown, map_type.currentText(), True))
         twod.toggled.connect(lambda: toggle_2d_3d(self.plot_data, sc_plot, twod, threed, "2d", map_type.currentText()))
         threed.toggled.connect(
             lambda: toggle_2d_3d(self.plot_data, sc_plot, threed, twod, "3d", map_type.currentText()))
@@ -136,8 +144,8 @@ class resultsWindow(QDialog):
         layout.setMenuBar(menubar)
         self.setLayout(layout)
         minsize = self.minimumSizeHint()
-        minsize.setHeight(self.minimumSizeHint().height() + 600)
-        minsize.setWidth(self.minimumSizeHint().width() + 600)
+        minsize.setHeight(self.minimumSizeHint().height() + 700)
+        minsize.setWidth(self.minimumSizeHint().width() + 700)
         self.setFixedSize(minsize)
 
     def reset_view(self):
@@ -147,14 +155,14 @@ class resultsWindow(QDialog):
         self.main_plot.axes.view_init(azim=-90, elev=89)
         self.main_plot.draw()
 
-    def loadFeaturefile(self, grouping, plot):
-        filename, dump = QFileDialog.getOpenFileName(self, 'Open File', '', 'Text files (*.txt)')
+    def loadFeaturefile(self, grouping, plot, new_plot):
+        filename, dump = QFileDialog.getOpenFileName(self, 'Open Feature File', '', 'Text files (*.txt)')
         if filename != '':
             self.feature_file.clear()
             self.feature_file.append(filename)
             print(self.feature_file)
 
-            self.data_filt(grouping, True, self.projection, plot, True)
+            self.data_filt(grouping, True, self.projection, plot, new_plot)
         else:
             load_featurefile_win = self.buildErrorWindow("Select Valid Feature File (.txt)", QMessageBox.Critical)
             load_featurefile_win.exec()
@@ -238,7 +246,8 @@ class resultsWindow(QDialog):
             dim=int(projection[0])
             #send to clustering.py for PCA, Sammon, t-SNE analysis
             title, xlabel, ylabel, P=plot_type(X, dim, plot)
-            self.plot_data.clear()
+            if len(self.plot_data)>0:
+                self.plot_data.clear()
 
             #save new x, y, z data and plot
             self.plot_data.append(P[:,0])
@@ -248,19 +257,20 @@ class resultsWindow(QDialog):
             else:
                 self.plot_data.append(np.zeros(len(self.plot_data[-1])))
         else:
-            for artist in self.main_plot.axes.collections:
-                artist.remove()
+            old_plots=self.plots[:]
+            for plot in old_plots:
+                plot.remove()
+        del self.plots[:]
         self.labels.clear()
         self.labels.extend(list(map(str, z)))
         #plot data
         colors= plt.cm.get_cmap('gist_ncar')(range(0, 255, floor(255/len(np.unique(self.labels)))))
-        plots=[]
         if len(np.unique(self.labels))>1:
             for label, i in zip(np.unique(self.labels), range(len(np.unique(self.labels)))):
-                plots.append(self.main_plot.axes.scatter3D(self.plot_data[0][np.array(self.labels)==label], self.plot_data[1][np.array(self.labels)==label], self.plot_data[2][np.array(self.labels)==label], label=label,
+                self.plots.append(self.main_plot.axes.scatter3D(self.plot_data[0][np.array(self.labels)==label], self.plot_data[1][np.array(self.labels)==label], self.plot_data[2][np.array(self.labels)==label], label=label,
                              s=10, alpha=1, color=[colors[i]], depthshade=False, picker=2, cmap=colors))
         else:
-            plots.append(self.main_plot.axes.scatter3D(self.plot_data[0], self.plot_data[1], self.plot_data[2], label=self.labels[0],
+            self.plots.append(self.main_plot.axes.scatter3D(self.plot_data[0], self.plot_data[1], self.plot_data[2], label=self.labels[0],
                                               s=10, alpha=1, color=[colors[0]], depthshade=False, picker=2, cmap=colors))
         #legend formating
         cols=2
@@ -284,9 +294,43 @@ class resultsWindow(QDialog):
             self.main_plot.axes.set_xlabel(xlabel)
             self.main_plot.axes.set_ylabel(ylabel)
             #save original x,y,z axis limits for resetview
-            for plot in plots:
-                self.original_xlim=[min(plot.axes.get_xlim3d()[0], self.original_xlim[0]), max(plot.axes.get_xlim3d()[1], self.original_xlim[1])]
-                self.original_ylim=[min(plot.axes.get_ylim3d()[0], self.original_ylim[0]), max(plot.axes.get_ylim3d()[1], self.original_ylim[1])]
-                self.original_zlim=[min(plot.axes.get_zlim3d()[0], self.original_zlim[0]), max(plot.axes.get_zlim3d()[1], self.original_zlim[1])]
+            self.original_xlim=[self.plots[-1].axes.get_xlim3d()[0], self.plots[-1].axes.get_xlim3d()[1]]
+            self.original_ylim=[self.plots[-1].axes.get_ylim3d()[0], self.plots[-1].axes.get_ylim3d()[1]]
+            self.original_zlim=[self.plots[-1].axes.get_zlim3d()[0], self.plots[-1].axes.get_zlim3d()[1]]
         self.main_plot.draw()
+    def save_file(self, map):
+        name = QFileDialog.getSaveFileName(self, 'Save File')[0]
+        if name:
+            for x in ['PCA','t-SNE','Sammon']:
+                print((x in name))
+                if (x in name) and x!=map:
+                    name=name.replace(x, '')
+                    print(name)
+            if name.find("txt")==-1:
+                name=name+".txt"
+            if name.find(map)==-1:
+                name=name.replace(".txt", map+".txt")
+            np.savetxt(name, np.concatenate((self.plot_data, self.original_xlim, self.original_ylim, self.original_zlim), axis=None), newline='', fmt='%10.5f')
+    def import_file(self, map_dropdown, colordropdown, threed):
+        filename= QFileDialog.getOpenFileName(self, 'Open Plot Data File', '', 'Text files (*.txt)')[0]
+        if filename != '':
+            print(filename)
+            new_data = np.loadtxt(filename)
+            self.plot_data= new_data[:-6].reshape(3, int((len(new_data)-6)/3))
+            self.original_xlim =[new_data[-6],new_data[-5]]
+            self.original_ylim = [new_data[-4], new_data[-3]]
+            self.original_zlim = [new_data[-2], new_data[-1]]
+            self.reset_view()
+            map=""
+            if np.all(self.plot_data[2]!=0):
+                threed.setChecked(True)
+            for x in ["PCA", "Sammon", "t-SNE"]:
+                if x in filename:
+                    map=x
+                    map_dropdown.setCurrentIndex(map_dropdown.findText(map))
+                    self.loadFeaturefile(colordropdown, map, False)
+                    break
+        else:
+            load_datafile_win = self.buildErrorWindow("Select Valid Data File (.txt)", QMessageBox.Critical)
+            load_datafile_win.exec()
 # end resultsWindow
